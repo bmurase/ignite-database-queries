21c0ab4a645cff96bf3cda1e2ca9b78c2ed45ab4
Beatriz Murase <biamurase@gmail.com>
2021-10-03 21:24:41 -0300

    :rocket: challenge finished

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,22 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      where: {id: user_id},
+      relations: ['games']
+    });
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('select * from users order by first_name asc');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`select * from users where lower(first_name) = lower('${first_name}') and lower(last_name) = lower('${last_name}')`);
   }
 }
